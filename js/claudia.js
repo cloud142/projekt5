0e75b7b926561d36a65f66430756fac676a615b9
cloud14 <clol48840@edu.ucl.dk>
2023-05-27 20:36:35 +0200

    js

--- js/claudia.js
+++ js/claudia.js
@@ -1,7 +1,5 @@
-// Array til at gemme formularfelter
-   const formFields = ['name', 'email', 'password'];
+  const formFields = ['name', 'email', 'password'];
 
-   // Array til at gemme fejlbeskeder
    const errorMessages = {
      name: 'Brugernavn er "1".',
      email: 'Email er "1@gmail.com".',
@@ -9,16 +7,16 @@
      password: 'Adganskode er "1".'
    };
 
-   // Function to validate the form
-   function validateForm(event) {
-     event.preventDefault(); // Prevent form submission
 
-     // Clear previous error messages
+   function validateForm(event) {
+     event.preventDefault();
+
+
      clearErrorMessages();
 
-     let isValid = true; // Flag to track form validity
+     let isValid = true;
 
-     // Validate each form field
+
      formFields.forEach(field => {
        const value = document.getElementById(field).value;
 
@@ -31,29 +29,27 @@
        }
      });
 
-     // If form is valid, submit the form
+
      if (isValid) {
        document.getElementById('myForm').submit();
      }
    }
 
-   // Function to check if email is valid using a simple regular expression
+
    function isValidEmail(email) {
      const emailRegex = /^\S+@\S+\.\S+$/;
      return emailRegex.test(email);
    }
 
-   // Function to display error message for a specific field
+
    function displayErrorMessage(field, message) {
      document.getElementById(`${field}Error`).textContent = message;
    }
 
-   // Function to clear all error messages
    function clearErrorMessages() {
      formFields.forEach(field => {
        document.getElementById(`${field}Error`).textContent = '';
      });
    }
 
-   // Event listener for form submission
    document.getElementById('myForm').addEventListener('submit', validateForm);
